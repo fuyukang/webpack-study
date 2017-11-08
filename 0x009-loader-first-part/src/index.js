6d6bb61af4bd361a2250daae1b4cbff312ffd6b4
FollowWinter <lyxxxx@yeah.net>
2017-11-09 02:06:11 +0800

    [更新] 修改文件夹名称

---
+++ 0x009-loader-first-part/src/index.js
@@ -0,0 +1,16 @@
+//raw-loader
+// var content = require('raw-loader!./index.txt')
+// var content = require('./index.txt')
+
+// file-loader
+// var funny = require('./../res/funny.png')
+// var funny2= require('./../res/funny2.png')
+// var funny2= require('./../res/icon.jpg')
+
+// url-loader
+// 低于81920
+require('./../res/icon.jpg')
+// 高于81920
+require('./../res/big.jpg')
+
+
---
+++ 0x009-loader-first-part/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+    entry : {
+        'index': ['./src/index.js'],
+    },
+    output: {
+        path    : path.join(__dirname, 'dist'),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.txt$/,
+                use : 'raw-loader'
+            },
+            // {
+            //     test: /\.(png|jpg|gif)$/,
+            //     use : 'file-loader'
+            // },
+            {
+                test: /\.(png|jpg|gif)$/,
+                // use : [
+                //     {
+                //         loader : 'url-loader',
+                //         options: {
+                //             limit   : 1048576, // 低于1m
+                //             name    : '[name].[hash].[ext]',
+                //             fallback: 'file-loader' //否则使用`file-loader`
+                //         }
+                //     }
+                // ]
+                use:'url-loader?limit=1045876&name=[name].[hash].[ext]'
+            }
+        ]
+    }
+};
