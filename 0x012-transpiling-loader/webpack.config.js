6deb53ad22e2869788c0423577c97682151fbcd5
FollowWinter <lyxxxx@yeah.net>
2017-11-09 16:34:20 +0800

    [更新] 添加bable-loader和awesome-typescript-loader的使用

---
+++ 0x012-transpiling-loader/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    entry  : {
+        'index': ['./src/index.ts'],
+    },
+    output : {
+        path    : path.join(__dirname, 'dist'),
+        filename: '[name].bundle.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+    module : {
+        rules: [
+            // {
+            //     test   : /\.js$/,
+            //     exclude: /node_modules/,
+            //     use    : {
+            //         loader: "babel-loader"
+            //     }
+            // },
+            {
+                test  : /\.tsx?$/,
+                loader: 'awesome-typescript-loader'
+            }
+        ]
+    }
+}
+;
